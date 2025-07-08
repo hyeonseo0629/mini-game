36785e2e1daa26536cfcecf63aaf652ef9b7c11b
byh <iambang12245@naver.com>
2025-07-08 10:32:05 +0900

    [UPDATE] import List class

--- src/main/java/com/hs/minigame/mapper/SampleMapper.java
+++ src/main/java/com/hs/minigame/mapper/SampleMapper.java
@@ -4,6 +4,8 @@ import com.hs.minigame.vo.SampleVO;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
+import java.util.List;
+
 // mapper -> sql
 @Mapper
 public interface SampleMapper {
