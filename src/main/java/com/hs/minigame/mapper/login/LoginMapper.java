8a567e1ae67c241a1c21a4066af5e438ea560c8e
byh <iambang12245@naver.com>
2025-07-14 09:10:22 +0900

    feat(login) : Login 기능을 위한 Controller, Mapper 수정

--- src/main/java/com/hs/minigame/mapper/login/LoginMapper.java
+++ src/main/java/com/hs/minigame/mapper/login/LoginMapper.java
@@ -8,10 +8,10 @@ import org.apache.ibatis.annotations.Select;
 @Mapper()
 public interface LoginMapper {
 
-    @Select("select user_id, user_pw, user_name, user_nickname, user_money from users where user_id =#{id}")
+    @Select("select user_no, user_id, user_pw, user_name, user_nickname, user_money from users where user_id =#{id}")
+
 
     UsersVO selectUser(String id);
-
 
     @Insert("insert into users(user_id, user_pw,user_name,user_nickname,user_money,user_role,user_avatar_img)"+
             "values (#{user_id},#{user_pw},#{user_name},#{user_nickname},#{user_money},#{user_role},#{user_avatar_img})"
