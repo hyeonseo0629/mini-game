c5f81f002d3a78912a19a78db3a837035083d541
hyeonseo <hyeonseo629@gmail.com>
2025-07-15 09:52:36 +0900

    no message

--- src/main/java/com/hs/minigame/controller/LoginC.java
+++ src/main/java/com/hs/minigame/controller/LoginC.java
@@ -7,16 +7,10 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
 
 @Controller
 public class LoginC {
